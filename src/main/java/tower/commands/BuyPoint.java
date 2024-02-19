664aa50b84a9ce68ed9fd6430fe32738d63f1499
Ayushkiller <malikayush999@gmail.com>
2024-02-19 18:34:49 +0530

    r

--- src/main/java/tower/commands/BuyPoint.java
+++ src/main/java/tower/commands/BuyPoint.java
@@ -9,14 +9,8 @@ import tower.Bundle;
 import tower.Players;
 import tower.Domain.Currency;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class BuyPoint {
-
-    private static final Map<Object, Integer> pointValues = new HashMap<>();
-
-
 
     public static void execute(Player player) {
         openMenu(player);
@@ -26,19 +20,19 @@ public class BuyPoint {
         String[][] buttons = new String[Currency.itemsforcore.length][Currency.itemsforcore[0].length];
 
         // Create button titles and point values for each item.
-        for (int i = 0; i < Currency.itemsforcore.length; i++) {
-            for (int j = 0; j < Currency.itemsforcore[i].length; j++) {
+        for (int i =  0; i < Currency.itemsforcore.length; i++) {
+            for (int j =  0; j < Currency.itemsforcore[i].length; j++) {
                 Item item = Currency.itemsforcore[i][j];
-                buttons[i][j] = String.format("[lime]%s (+%d)", item.name, pointValues.get(item));
+                buttons[i][j] = String.format("[lime]%s (+%d)", item.name, Currency.Gain[i][j]);
             }
         }
 
         Call.menu(player.con, Menus.registerMenu((player1, option) -> {
-            Item chosenItem = Currency.itemsforcore[option / buttons[0].length][option % buttons[0].length];
-            int pointsToAdd = pointValues.get(chosenItem);
             PlayerData playerData = Players.getPlayer(player);
+            int pointsToAdd = Currency.Gain[option / Currency.itemsforcore[0].length][option % Currency.itemsforcore[0].length];
             playerData.addPoints(pointsToAdd);
             player.sendMessage(Bundle.get("menu.buypoint.success"));
         }), Bundle.get("menu.buypoint.title", player.locale()), "", buttons);
     }
 }
+
