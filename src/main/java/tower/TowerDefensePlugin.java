cbffe9e89879bea2953273fdf5a7574e9714cf81
Ayushkiller <malikayush999@gmail.com>
2024-02-29 12:30:41 +0530

    hh

--- src/main/java/tower/TowerDefensePlugin.java
+++ src/main/java/tower/TowerDefensePlugin.java
@@ -1,8 +1,6 @@
 package tower;
 
 import arc.util.CommandHandler;
-import mindustry.core.GameState;
-import mindustry.core.GameState.State;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 import tower.game.Loader;
@@ -10,9 +8,7 @@ import tower.menus.Menu;
 import tower.pathing.TowerPathfinder;
 import useful.Bundle;
 
-
 public class TowerDefensePlugin extends Plugin {
-
 
     @Override
     public void init() {
@@ -20,20 +16,12 @@ public class TowerDefensePlugin extends Plugin {
         TowerPathfinder.init();
         PluginLogic.init();
         Loader.load();
-        state = new GameState();
     }
-     private GameState state; 
 
-    private void info(String message) {
-        System.out.println(message);
-    }
+
     public void registerClientCommands(CommandHandler handler) {
         handler.register("menu", "Opens a menu", (String[] args, Player player) -> Menu.execute(player));
-    handler.register("pause", "<on/off>", "Pause or unpause the game.", arg -> {
-    boolean pause = arg[0].equals("on");
-    state.set(state.isPaused() ? State.playing : State.paused);
-    info(pause ? "Game paused." : "Game unpaused.");
-     });
 
     }
+
 }
