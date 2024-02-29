0514d40d265f8e98bda1c3a3b7db34db771b4a12
Ayushkiller <malikayush999@gmail.com>
2024-02-29 11:07:18 +0530

    h

--- src/main/java/tower/TowerDefensePlugin.java
+++ src/main/java/tower/TowerDefensePlugin.java
@@ -1,6 +1,8 @@
 package tower;
 
 import arc.util.CommandHandler;
+import mindustry.core.GameState;
+import mindustry.core.GameState.State;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 import tower.game.Loader;
@@ -19,8 +21,24 @@ public class TowerDefensePlugin extends Plugin {
         PluginLogic.init();
         Loader.load();
     }
+     private GameState state; 
+     private void err(String message) {
+        System.err.println(message);
+    }
+    private void info(String message) {
+        System.out.println(message);
+    }
     public void registerClientCommands(CommandHandler handler) {
         handler.register("menu", "Opens a menu", (String[] args, Player player) -> Menu.execute(player));
+    handler.register("pause", "<on/off>", "Pause or unpause the game.", arg -> {
+    if(state.isMenu()){
+        err("Cannot pause without a game running.");
+        return;
+    }
+    boolean pause = arg[0].equals("on");
+    state.set(state.isPaused() ? State.playing : State.paused);
+    info(pause ? "Game paused." : "Game unpaused.");
+     });
 
     }
 }
