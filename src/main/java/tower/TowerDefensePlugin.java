f899648f1558ab87e62fcf2e4222b2b1a1eaecbc
Ayushkiller <malikayush999@gmail.com>
2024-02-29 11:26:11 +0530

    h

--- src/main/java/tower/TowerDefensePlugin.java
+++ src/main/java/tower/TowerDefensePlugin.java
@@ -20,21 +20,16 @@ public class TowerDefensePlugin extends Plugin {
         TowerPathfinder.init();
         PluginLogic.init();
         Loader.load();
+        state = new GameState();
     }
      private GameState state; 
-     private void err(String message) {
-        System.err.println(message);
-    }
+
     private void info(String message) {
         System.out.println(message);
     }
     public void registerClientCommands(CommandHandler handler) {
         handler.register("menu", "Opens a menu", (String[] args, Player player) -> Menu.execute(player));
     handler.register("pause", "<on/off>", "Pause or unpause the game.", arg -> {
-    if(state.isMenu()){
-        err("Cannot pause without a game running.");
-        return;
-    }
     boolean pause = arg[0].equals("on");
     state.set(state.isPaused() ? State.playing : State.paused);
     info(pause ? "Game paused." : "Game unpaused.");
