9d39d230d2d3691bb23eeac06486984992aa6659
Ayushkiller <malikayush999@gmail.com>
2024-02-29 18:06:39 +0530

    H

--- src/main/java/tower/Domain/PlayerData.java
+++ src/main/java/tower/Domain/PlayerData.java
@@ -105,6 +105,6 @@ public class PlayerData {
         player.ip();
         this.hp = player.unit().health();
         this.name = player.name();
-        this.points= 1000;
+        this.points= 10;
     }
 }
