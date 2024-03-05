272c9b12ac916a2092cb26788b06be2d0e3406d6
Ayushkiller <malikayush999@gmail.com>
2024-03-05 15:33:02 +0530

    ht

--- src/main/java/tower/Domain/Effects.java
+++ src/main/java/tower/Domain/Effects.java
@@ -11,19 +11,19 @@ public class Effects {
     public static List<Map<String, Object>> effects = new ArrayList<>();
 
     static {
-        effects.add(Map.of("effect", StatusEffects.overdrive, "price",  20));
-        effects.add(Map.of("effect", StatusEffects.fast, "price",  15));
-        effects.add(Map.of("effect", StatusEffects.overclock, "price",  30));
-        effects.add(Map.of("effect", StatusEffects.shielded, "price",  20));
-        effects.add(Map.of("effect", StatusEffects.boss, "price",  45));
-        effects.add(Map.of("effect", StatusEffects.invincible, "price",  30));
-        effects.add(Map.of("effect", StatusEffects.unmoving, "price",  0));
-        effects.add(Map.of("effect", StatusEffects.overdrive, "price", 40, "healthMultiplier", 4f, "speedMultiplier", 1.2f, "damageMultiplier", 1.5f, "name", "Overdrive Enhanced"));
-        effects.add(Map.of("effect", StatusEffects.fast, "price", 30, "speedMultiplier", 4f, "name", "Fast Enhanced"));
-        effects.add(Map.of("effect", StatusEffects.overclock, "price", 50, "speedMultiplier", 1.3f, "damageMultiplier", 2f, "reloadMultiplier", 4f, "name", "Overclock Enhanced"));
-        effects.add(Map.of("effect", StatusEffects.shielded, "price", 40, "healthMultiplier", 4f, "name", "Shielded Enhanced"));
-        effects.add(Map.of("effect", StatusEffects.boss, "price", 60, "damageMultiplier", 3f, "healthMultiplier", 2f, "name", "Boss Enhanced"));
-        effects.add(Map.of("effect", StatusEffects.invincible, "price", 50, "healthMultiplier", 5f, "name", "Invincible Enhanced"));
+        effects.add(Map.of("name", "Overdrive", "effect", StatusEffects.overdrive, "price",  20));
+        effects.add(Map.of("name", "Fast", "effect", StatusEffects.fast, "price",  15));
+        effects.add(Map.of("name", "Overclock", "effect", StatusEffects.overclock, "price",  30));
+        effects.add(Map.of("name", "Shielded", "effect", StatusEffects.shielded, "price",  20));
+        effects.add(Map.of("name", "Boss", "effect", StatusEffects.boss, "price",  45));
+        effects.add(Map.of("name", "Invincible", "effect", StatusEffects.invincible, "price",  30));
+        effects.add(Map.of("name", "Unmoving", "effect", StatusEffects.unmoving, "price",  0));
+        effects.add(Map.of("name", "Overdrive Buffed", "effect", CustomStatusEffects.overdriveBuffed, "price",  40));
+        effects.add(Map.of("name", "Overclock Buffed", "effect", CustomStatusEffects.overclockBuffed, "price",  50));
+        effects.add(Map.of("name", "Shielded Buffed", "effect", CustomStatusEffects.shieldedBuffed, "price",  40));
+        effects.add(Map.of("name", "Fast Buffed", "effect", CustomStatusEffects.speedBuffed, "price",  40));
+        effects.add(Map.of("name", "Boss Buffed", "effect", CustomStatusEffects.bossBuffed, "price",  65));
+        effects.add(Map.of("name", "Invincible Buffed", "effect", CustomStatusEffects.invincibleBuffed, "price",  50));
     }
 }
 
--- src/main/java/tower/game/Loader.java
+++ src/main/java/tower/game/Loader.java
@@ -1,10 +1,11 @@
 package tower.game;
 
+import tower.Domain.CustomStatusEffects;
 import tower.commands.Units;
 
 public class Loader {
     public static void load() {
-
+        CustomStatusEffects.load();
         EventLoader.init();
         Units.initUnitsTable();
 
