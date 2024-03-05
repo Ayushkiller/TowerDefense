57da535739ac204266d360a344eae27637bed17b
Ayushkiller <malikayush999@gmail.com>
2024-03-05 11:32:15 +0530

    h

--- src/main/java/tower/Blocks/Force.java
+++ src/main/java/tower/Blocks/Force.java
@@ -1,33 +1,25 @@
 package tower.Blocks;
 
-import arc.struct.Seq;
-import mindustry.gen.Groups;
 import mindustry.gen.Unit;
+import java.util.Map;
 import mindustry.world.Tile;
 import mindustry.world.blocks.defense.ForceProjector;
 import tower.PluginLogic;
-
+import tower.Domain.UnitsTable;
 public class Force {
 
-    public static Seq<Unit> getNearbyUnits(Tile tile, float radius) {
-        Seq<Unit> nearbyUnits = new Seq<>();
-        for (Unit unit : Groups.unit) {
-            if (unit.dst(tile.worldx(), tile.worldy()) <= radius) {
-                nearbyUnits.add(unit);
-            }
-        }
-        return nearbyUnits;
-    }
+   
 
     public static void applySlowEffectToUnits(Tile tile) {
         if (tile.build instanceof ForceProjector.ForceBuild) {
             ForceProjector.ForceBuild forceProjector = (ForceProjector.ForceBuild) tile.build;
             float realRadius = forceProjector.realRadius();
             if (realRadius > 0 && !forceProjector.broken) {
-                Seq<Unit> units = getNearbyUnits(tile, realRadius);
-                for (Unit unit : units) {
+                for (Map<String, Object> unitMap : UnitsTable.units) {
+                    // Assuming you have a way to get the Unit object from the map
+                    Unit unit = (Unit) unitMap.get("unit");
                     if (unit.team != forceProjector.team) {
-                        unit.speedMultiplier(0.4f * PluginLogic.multiplier);
+                        unit.speedMultiplier(0f * PluginLogic.multiplier);
                     }
                 }
             }
