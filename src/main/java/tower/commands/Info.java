95248caea28b35625b06f54ce6c87351f9f4c9f9
Ayushkiller <malikayush999@gmail.com>
2024-03-06 10:44:19 +0530

    hm

--- src/main/java/tower/commands/Info.java
+++ src/main/java/tower/commands/Info.java
@@ -4,19 +4,12 @@ import mindustry.gen.Call;
 import mindustry.gen.Player;
 import mindustry.ui.Menus;
 import tower.Bundle;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Info {
     private static final List<String> infoKeys = Arrays.asList("settings.Overview", "info.Buypoint", "info.Powerup", "info.SuperPow", "info.Points", "info.Units");
-    private static final Map<String, String[]> buttons = infoKeys.stream()
-            .collect(Collectors.toMap(
-                    key -> key.split("\\.")[1], // Extract the part after "info." as label
-                    key -> new String[]{key.split("\\.")[1], key} // Each button has a label and an associated key
-            ));
-
     private static int currentPage = 0; // Store the current page as a class-level variable
 
     private static int registerMenu() {
@@ -34,7 +27,11 @@ public class Info {
     }
 
     private static void displayMenu(Player player) {
-        Call.menu(player.con, registerMenu(), Bundle.get("settings.title", player.locale), "", buttons.values().toArray(new String[0][]));
+        String[][] buttonArray = new String[][]{
+                {"Next"},
+                {"Close"}
+        };
+        Call.menu(player.con, registerMenu(), Bundle.get("settings.title", player.locale), "", buttonArray);
     }
 
     private static void navigateToNextPage(Player player) {
