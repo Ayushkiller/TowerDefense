c66bdeea645aa74c967da7f73857b20008535cc3
Ayushkiller <malikayush999@gmail.com>
2024-03-06 11:05:10 +0530

    hk

--- src/main/java/tower/commands/Info.java
+++ src/main/java/tower/commands/Info.java
@@ -5,13 +5,11 @@ import mindustry.gen.Player;
 import mindustry.ui.Menus;
 import tower.Bundle;
 
+import java.util.logging.Logger;
+
 public class Info {
-    private static final int menu = Menus.registerMenu((player, option) -> {
-        switch (option) {
-            case 1 -> Menuforunits.execute(player); // Back
-            case 2 -> nextMessage(player); // Next
-        }
-    });
+    private static final Logger logger = Logger.getLogger(Info.class.getName());
+    private static final int menu = Menus.registerMenu(Info::handleMenuOption);
     private static final String[][] buttons = {
         {"[gray]Close"},
         {"[accent]Next"}
@@ -20,20 +18,24 @@ public class Info {
     private static String[] messages;
 
     public static void execute(Player player) {
+        logger.info("Executing Info command for player: " + player.name);
         initializeMessages(player.locale);
         openGui(player);
     }
 
     public static void openGui(Player player) {
+        logger.info("Opening GUI for player: " + player.name);
         Call.menu(player.con, menu, Bundle.get("settings.title", player.locale), messages[currentMessageIndex], buttons);
     }
 
     private static void nextMessage(Player player) {
         currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+        logger.info("Displaying next message for player: " + player.name);
         openGui(player);
     }
 
     private static void initializeMessages(String locale) {
+        logger.info("Initializing messages for locale: " + locale);
         messages = new String[]{
             Bundle.get("settings.Overview", locale),
             Bundle.get("info.Buypoint", locale),
@@ -42,5 +44,13 @@ public class Info {
             Bundle.get("info.Points", locale),
             Bundle.get("info.Units", locale)
         };
+    }
+
+    private static void handleMenuOption(Player player, int option) {
+        logger.info("Handling menu option for player: " + player.name + ", option: " + option);
+        switch (option) {
+            case 1 -> Menuforunits.execute(player); // Back
+            case 2 -> nextMessage(player); // Next
+        }
     }
 }
