31fa4599e5c56625751f3f48fe129155b2e5c954
Ayushkiller <malikayush999@gmail.com>
2024-03-08 17:05:24 +0530

    hmmmmmmmm

--- src/main/java/tower/Domain/Abilities.java
+++ src/main/java/tower/Domain/Abilities.java
@@ -2,49 +2,51 @@ package tower.Domain;
 
 import mindustry.content.StatusEffects;
 import mindustry.content.UnitTypes;
+import mindustry.entities.abilities.Ability;
 import mindustry.entities.abilities.StatusFieldAbility;
 import mindustry.entities.abilities.UnitSpawnAbility;
 import tower.commands.Abilityies.CustomStatusFieldAbility;
-
 import java.util.List;
 import java.util.Arrays;
 
 public class Abilities {
-    public static List<StatusFieldAbility> getAbility1() {
+    public static List<Ability> getAbility1() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 20f;
         return Arrays.asList(
-                new StatusFieldAbility(StatusEffects.boss, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.sporeSlowed, duration,cooldown,range));
+                new StatusFieldAbility(StatusEffects.boss, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.sporeSlowed, duration, cooldown, range));
     }
-    public static List<StatusFieldAbility> getAbility2() {
+
+    public static List<Ability> getAbility2() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 30f;
         return Arrays.asList(
-                new StatusFieldAbility(StatusEffects.overclock, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.electrified, duration,cooldown,range));
+                new StatusFieldAbility(StatusEffects.overclock, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.electrified, duration, cooldown, range));
     }
 
-    public static List<StatusFieldAbility> getAbility3() {
+    public static List<Ability> getAbility3() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 60f;
         return Arrays.asList(
-                new CustomStatusFieldAbility(StatusEffects.sapped, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.burning, duration,cooldown,range));
+                new CustomStatusFieldAbility(StatusEffects.sapped, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.burning, duration, cooldown, range));
     }
-    public static List<StatusFieldAbility> getAbility4() {
+
+    public static List<Ability> getAbility4() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 60f;
         return Arrays.asList(
-                new CustomStatusFieldAbility(StatusEffects.sporeSlowed, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.blasted, duration,cooldown,range));
+                new CustomStatusFieldAbility(StatusEffects.sporeSlowed, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.blasted, duration, cooldown, range));
     }
 
-    public static List<UnitSpawnAbility> getAbility6() {
+    public static List<Ability> getAbility6() {
         float spawnTime = 60f * 45f;
         float x1 = 19.25f;
         float y1 = -31.75f;
@@ -55,40 +57,41 @@ public class Abilities {
                 new UnitSpawnAbility(UnitTypes.zenith, spawnTime, x1, y1),
                 new UnitSpawnAbility(UnitTypes.zenith, spawnTime, x2, y2));
     }
-    public static List<StatusFieldAbility> getAbility7() {
+
+    public static List<Ability> getAbility7() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 30f;
         return Arrays.asList(
-                new StatusFieldAbility(StatusEffects.overclock, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.melting, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.electrified, duration,cooldown,range));
-
+                new StatusFieldAbility(StatusEffects.overclock, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.melting, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.electrified, duration, cooldown, range));
     }
-    public static List<StatusFieldAbility> getAbility8() {
+
+    public static List<Ability> getAbility8() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 15f;
         return Arrays.asList(
-                new CustomStatusFieldAbility(StatusEffects.melting, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.shocked, duration,cooldown,range));
+                new CustomStatusFieldAbility(StatusEffects.melting, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.shocked, duration, cooldown, range));
     }
-    public static List<StatusFieldAbility> getAbility9() {
+
+    public static List<Ability> getAbility9() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 30f;
         return Arrays.asList(
-                new StatusFieldAbility(StatusEffects.fast, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.freezing, duration,cooldown,range));
-
+                new StatusFieldAbility(StatusEffects.fast, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.freezing, duration, cooldown, range));
     }
-    public static List<StatusFieldAbility> getAbility10() {
+
+    public static List<Ability> getAbility10() {
         float duration = 60f * 6;
         float cooldown = 60f * 6f;
         float range = 30f;
         return Arrays.asList(
-                new StatusFieldAbility(StatusEffects.overdrive, duration,cooldown,range),
-                new CustomStatusFieldAbility(StatusEffects.blasted, duration,cooldown,range));
-
+                new StatusFieldAbility(StatusEffects.overdrive, duration, cooldown, range),
+                new CustomStatusFieldAbility(StatusEffects.blasted, duration, cooldown, range));
     }
 }
--- src/main/java/tower/commands/Abilityies/CustomStatusFieldAbility.java
+++ src/main/java/tower/commands/Abilityies/CustomStatusFieldAbility.java
@@ -1,27 +1,54 @@
 package tower.commands.Abilityies;
 
 import arc.math.*;
+import arc.scene.ui.layout.Table;
 import arc.util.*;
+import mindustry.content.*;
 import mindustry.entities.*;
-import mindustry.entities.abilities.StatusFieldAbility;
+import mindustry.entities.abilities.Ability;
 import mindustry.gen.*;
 import mindustry.type.*;
+import mindustry.world.meta.*;
 
-public class CustomStatusFieldAbility extends StatusFieldAbility {
-    public CustomStatusFieldAbility(StatusEffect effect, float duration, float reload, float range) {
-        super(effect, duration, reload, range);
+import static mindustry.Vars.tilesize;
+
+public class CustomStatusFieldAbility extends Ability{
+    public StatusEffect effect;
+    public float duration = 60, reload = 100, range = 20; //
+    public boolean onShoot = false;
+    public Effect applyEffect = Fx.none;
+    public Effect activeEffect = Fx.overdriveWave;
+    public float effectX, effectY;
+    public boolean parentizeEffects, effectSizeParam = true;
+
+    protected float timer;
+
+    CustomStatusFieldAbility(){}
+
+    public CustomStatusFieldAbility(StatusEffect effect, float duration, float reload, float range){
+        this.duration = duration;
+        this.reload = reload;
+        this.range = range;
+        this.effect = effect;
     }
 
     @Override
-    public void update(Unit unit) {
+    public void addStats(Table t){
+        t.add("[lightgray]" + Stat.reload.localized() + ": [white]" + Strings.autoFixed(60f / reload, 2) + " " + StatUnit.perSecond.localized());
+        t.row();
+        t.add("[lightgray]" + Stat.shootRange.localized() + ": [white]" +  Strings.autoFixed(range / tilesize, 2) + " " + StatUnit.blocks.localized());
+        t.row();
+        t.add(effect.emoji() + " " + effect.localizedName);
+    }
+
+    @Override
+    public void update(Unit unit){
         timer += Time.delta;
 
         if(timer >= reload && (!onShoot || unit.isShooting)){
             Units.nearby(unit.team, unit.x, unit.y, range, other -> {
-                if(unit.team != other.team) {
-                    other.apply(effect, duration);
-                    applyEffect.at(other, parentizeEffects);
-                }
+                other.apply(effect, duration);
+                applyEffect.at(other, parentizeEffects);
             });
 
             float x = unit.x + Angles.trnsx(unit.rotation, effectY, effectX), y = unit.y + Angles.trnsy(unit.rotation, effectY, effectX);
