8201abde649ad9a1b40a3815408ea789269217fc
Ayushkiller <malikayush999@gmail.com>
2024-03-21 20:33:35 +0530

    h

--- src/main/java/tower/PluginLogic.java
+++ src/main/java/tower/PluginLogic.java
@@ -317,7 +317,7 @@ public class PluginLogic {
                 event.unit.speedMultiplier(event.unit.speedMultiplier * multiplier);
                 event.unit.type.mineWalls = event.unit.type.mineFloor = event.unit.type.targetAir = event.unit.type.targetGround = false;
                 event.unit.type.payloadCapacity = event.unit.type.legSplashDamage = event.unit.type.range = event.unit.type.maxRange = event.unit.type.mineRange = 0f;
-                event.unit.type.aiController = event.unit.type.naval ? GroundAI::new : FlyingAIForAss::new;
+                event.unit.type.aiController = (event.unit.type.naval || event.unit.type.canDrown) ? GroundAI::new : FlyingAIForAss::new;;
                 event.unit.type.targetFlags = new BlockFlag[] { BlockFlag.core };
             }
         });
